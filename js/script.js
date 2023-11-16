3e069d1886cdf711333c8b1c929b067d6105d3e5
I am <shamil2.718@ya.ru>
2023-11-16 19:46:18 +0300

    trening-commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use string';
+
+//   ИНКРЕМЕНТ ++
+
+
+
+// дикремент --
+
+
+
+let incr = 10,
+	decr = 123;
+    
+const isChecked = !false,
+	isClose = !false;
+
+console.log(isChecked || isClose);
